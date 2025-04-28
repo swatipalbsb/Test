3f0e15c928ef3203ab90acbfd036daea51ddcd3b
swatipalbsb <swatipal790703@gmail.com>
2025-04-28 23:49:22 +0530

    Add files via upload
    Added arithmetic program

---
+++ Arithmatic.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class Arithmatic
+{
+public static void main(String arg[])
+{
+float a=14.8f,b=5.3f,c=6.2f;
+float d=a/b*c;
+float r=a%b+c;
+System.out.println(d);
+System.out.println(r);
+}
+}
